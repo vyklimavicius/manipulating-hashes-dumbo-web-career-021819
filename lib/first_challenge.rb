3465ba06896dff80c03f872efe3207f8f9e20c5b
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-02 21:56:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,10 +12,12 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
+  
+ contacts["Freddy Mercury"].each do |attribute, data|
+	if attribute == :favorite_icecream_flavors
+			data.delete("strawberry")
+		end
+	end
   #remember to return your newly altered contacts hash!
   contacts
 end
